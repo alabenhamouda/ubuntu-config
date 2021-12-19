8b2cc1fa49a228106689d29b96f2edab3981dea9
alabh <alabenhmouda@gmail.com>
2021-12-20 00:01:54 +0100

    add cp template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define PI 3.14159265359
+#define lp(i, n) for(size_t i = 0; i < n; i++)
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+const char nl = '\n';
+
+void solve(){
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t; cin >> t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
